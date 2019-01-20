279c2ad02d916c99d439cbb535af25249b435cca
thxawa <thxawa07@gmail.com>
2019-01-20 12:16:51 +0630

    Edit two file

--- app/src/main/java/com/gusto/student2/MainActivity.java
+++ app/src/main/java/com/gusto/student2/MainActivity.java
@@ -25,14 +25,14 @@ public class MainActivity extends AppCompatActivity {
         name_tv = findViewById(R.id.tv_name);
         name_tv.setText("Hello Gusto Student");
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 
     @Override
